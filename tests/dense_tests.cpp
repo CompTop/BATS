28117636533294465676a65b432cb1fe1e59789b
Anjan Dwaraknath <anjandn@gmail.com>
2019-10-26 21:13:47 -0700

    Add test for el_commute

--- tests/dense_tests.cpp
+++ tests/dense_tests.cpp
@@ -60,12 +60,31 @@ TEST_CASE("RowView") {
 	a1.r(2)=a2.r(3);
 
 	F3 resv[] = {
-	0,0,0,0,
-	0,0,1,0,
+	0,0,2,0,
 	0,0,1,0,
 	0,0,2,0,
+	0,0,1,0,
 	};
 	AD res(4,4,resv);
 
 	CHECK( (a1==res) );
 }
+
+TEST_CASE("el_commute") {
+	int elmat1v[] = {1,0,0,0, 
+		          0,1,0,0,
+		          0,0,0,1,
+		          0,0,0,0};
+	EL<Dense<int>> elmat1(4,4,elmat1v);
+
+	int lmat1v[] = {2,2,3,4, 
+		          0,3,2,4,
+		          0,0,4,4,
+		          0,0,0,5};
+	L<Dense<int>> lmat1(4,4,lmat1v);
+
+	auto res = el_commute(elmat1,lmat1);
+
+	CHECK( (matmul(res,elmat1)== matmul(elmat1,lmat1)) );
+}
+
