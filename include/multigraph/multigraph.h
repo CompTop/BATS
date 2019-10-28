78ed4c548cba2c2a0b01166309d6d9fe81666363
Brad Nelson <bjnelson@stanford.edu>
2019-10-28 15:55:40 -0700

    multigraph class sketch

--- include/multigraph/multigraph.h
+++ include/multigraph/multigraph.h
@@ -1,67 +1,77 @@
 #include <vector>
 #include <iostream>
 
-// node type templated over data
-// store incoming and outgoing edges
+// Multigraph type
+// template over node data, edge data
+// inner types for nodes and edges
+// multigraph is not responsible for data - always pointer to data
 
-// edge type templated over data
-// store source, target, and data
-
-template <typename T>
-struct Node {
-private:
-    T* data;
-    std::vector< void* > inputs;
-    std::vector< void* > outputs;
-
+template <typename NT, typename ET>
+class MultiGraph {
 public:
-    Node(T &x) : data(&x) {};
+    struct Node;
+    struct Edge;
 
-    //inline void add_input(ET in) { inputs.emplace_back(&in); }
-    template <typename ET>
-    inline void add_input(ET* in) { inputs.emplace_back((void*) in); }
-    //inline void add_output(ET out) { outputs.emplace_back(&out); }
-    template <typename ET>
-    inline void add_output(ET* out) { outputs.emplace_back((void*) out); }
+    struct Node {
+        NT* data;
+        std::vector<Edge*> input;
+        std::vector<Edge*> output;
 
-    inline T* get_data() { return data; }
+        Node(NT& x) : data(&x) {};
+        Node(NT* x) : data( x) {};
 
-    void print() {
-        std::cout << '[' << this << "] Node : " << std::endl;
-        std::cout << "  [" << data << "] data : " << *data << std::endl;
-        std::cout << "  " << inputs.size() << " inputs" << std::endl;
-        std::cout << "  " << outputs.size() << " outputs" << std::endl;
+        //inline void add_input(ET in) { inputs.emplace_back(&in); }
+        inline void add_input(Edge* in) { input.emplace_back(in); }
+        //inline void add_output(ET out) { outputs.emplace_back(&out); }
+        inline void add_output(Edge* out) { output.emplace_back(out); }
 
-    }
-};
+        inline NT* get_data() { return data; }
 
-// template over T - data type
-template <typename T>
-struct Edge {
-private:
-    void* src;
-    void* targ;
-    T* data;
-
-public:
-
-    template <typename NT>
-    Edge(Node<NT> &sin, Node<NT> &tin, T& x) : src((void*) &sin), targ((void*) &tin), data(&x) {
-        // add edge to src outputs
-        sin.add_output(this);
-        // add edge to targ inputs
-        tin.add_input(this);
+        void print() {
+            std::cout << '[' << this << "] Node : " << std::endl;
+            std::cout << "  [" << data << "] data : " << *data << std::endl;
+            std::cout << "  " << input.size() << " inputs" << std::endl;
+            std::cout << "  " << output.size() << " outputs" << std::endl;
+        }
     };
 
-    inline T* get_data() { return data; }
+    struct Edge {
+        ET* data;
+        Node* src;
+        Node* targ;
 
+        Edge(Node& sin, Node& tin, ET& x) : src(&sin), targ(&tin), data(&x) {
+            // add edge to src outputs
+            sin.add_output(this);
+            // add edge to targ inputs
+            tin.add_input(this);
+        }
 
+        inline ET* get_data() { return data; }
 
-    void print() {
-        std::cout << '[' << this << "] Edge : " << std::endl;
-        std::cout << "  [" << src << "] source" << std::endl;
-        std::cout << "  [" << targ << "] target" << std::endl;
-        std::cout << "  [" << data << "] data " << *data << std::endl;
+        void print() {
+            std::cout << '[' << this << "] Edge : " << std::endl;
+            std::cout << "  [" << src << "] source" << std::endl;
+            std::cout << "  [" << targ << "] target" << std::endl;
+            std::cout << "  [" << data << "] data " << *data << std::endl;
+        }
+    };
+
+private:
+    std::vector<Node> node;
+    std::vector<Edge> edge;
+
+public:
+
+    MultiGraph() {}
+
+    Node& add_node(NT &a) {
+        node.emplace_back(Node(&a));
+        return node.back();
     }
 
+    Edge& add_edge(Node &a, Node &b, ET &data) {
+        edge.emplace_back(Edge(a, b, data));
+        return edge.back();
+    }
 };
