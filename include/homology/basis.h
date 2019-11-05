78ee7443802a81da9866e8fb3991c931c0f31215
Brad Nelson <bjnelson@stanford.edu>
2019-11-05 12:03:32 -0800

    preferred representative is now a method of the basis

--- include/homology/basis.h
+++ include/homology/basis.h
@@ -39,4 +39,40 @@ struct ReducedChainComplex {
 	// size of homology vector space in dimension k
 	inline size_t hdim(size_t k) const { return I[k].size(); }
 	inline size_t maxdim() const { return R.size() - 1; }
+
+	// modify y in-place to be preferred representative for homology class in dimension k
+	template <class TVec>
+	void find_preferred_representative(
+		TVec &y,
+		const size_t k
+	) const {
+		if (k == maxdim()) {
+			// all cycles generate homology, so nothing to do
+			return;
+		}
+		// else we need to find the preferred representative
+		// const ColumnMatrix<TVec> &bdry,
+		// const p2c_type &p2c
+		size_t j = R[k+1].nrow();
+		auto yit = y.upper_bound(j); // find iterator past last nonzero
+		while (yit != y.nzbegin()) {
+			--yit;
+			j = yit->ind;
+			// find if j is a pivot of bdry
+			if (p2c[k+1].count(j) > 0) {
+				auto i = p2c[k+1].at(j);
+
+				// form column i of boundary in homology revealing basis
+				auto bdri = u_solve(U[k], R[k+1][i]);
+				auto ipiv = bdri.lastnz();
+				auto a = (yit->val) / ipiv.val;
+				y.axpy(-a, bdri);
+
+				// get next nonzero
+				yit = y.upper_bound(j-1);
+			}
+			// else j is a preferred representative
+			// so we do nothing
+		}
+	}
 };
