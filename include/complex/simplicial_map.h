cff71b935ce50e9bec9327323567b8d81e8cae7c
Brad Nelson <bjnelson@stanford.edu>
2020-08-28 14:11:26 -0500

    simplicial inclusion map

--- include/complex/simplicial_map.h
+++ include/complex/simplicial_map.h
@@ -54,3 +54,34 @@ CellularMap SimplicialMap(
 
 	return f;
 }
+
+
+// construct simplicial map from X to Y
+// no initial data is specified - assume inclusion map
+CellularMap SimplicialMap(
+	const SimplicialComplex &X,
+	const SimplicialComplex &Y
+) {
+	std::vector<size_t> s; // simplex
+	size_t maxd = X.maxdim();
+	CellularMap f(maxd);
+	for (size_t k = 0; k < maxd+1; k++) {
+		std::vector<SparseVector<int, size_t>> col;
+		for (size_t i = 0; i < X.ncells(k); i++) {
+			// fill s with image of simplex i in dimension k
+			s.clear();
+			for (auto it = X.simplex_begin(k, i); it != X.simplex_end(k, i); ++it) {
+				s.emplace_back(*it);
+			}
+
+			size_t j = Y.find_idx(s);
+			if (j == bats::NO_IND) {
+				throw std::out_of_range("No existing target simplex!");
+			}
+			col.emplace_back(SparseVector<int, size_t>({j}, {1}));
+		}
+		f[k] = ColumnMatrix<SparseVector<int, size_t>>(Y.ncells(k), X.ncells(k), col);
+	}
+
+	return f;
+}
