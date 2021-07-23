b992ac432cc6b47d95002fff5ff74dca3a5f9346
Brad Nelson <bjnelson@stanford.edu>
2021-07-23 13:58:23 -0500

    cubical levelset extensions

--- include/filtration/extension.hpp
+++ include/filtration/extension.hpp
@@ -3,6 +3,7 @@
 #include <functional>
 #include <vector>
 #include <utility>
+#include <complex/cubical_complex.hpp>
 
 /*
 extend filtrations
@@ -39,4 +40,86 @@ auto lower_star_filtration(
         return std::make_tuple(f0[*it], *it);
     };
     return extend_filtration(X, filtfn);
+}
+
+/**
+return maximum vertex value on a cube c
+assume 2 dimensions
+*/
+template <typename T>
+auto max_cube_val(
+    const std::vector<size_t>& c,
+    const std::vector<std::vector<T>>& f0
+) {
+    return std::max(
+        std::max(
+            f0[c[0]][c[2]],
+            f0[c[0]][c[3]]),
+        std::max(
+            f0[c[1]][c[2]],
+            f0[c[1]][c[3]])
+    );
+}
+
+/**
+return maximum vertex value on a cube c
+assume 3 dimensions
+*/
+template <typename T>
+auto max_cube_val(
+    const std::vector<size_t>& c,
+    const std::vector<std::vector<std::vector<T>>>& f0
+) {
+    T val = f0[c[0]][c[2]][c[4]];
+    for (size_t i : {0, 1}) {
+        for (size_t j : {2, 3}) {
+            for (size_t k : {4, 5}) {
+                T tmp = f0[c[i]][c[j]][c[k]];
+                val = (tmp > val) ? tmp : val;
+            }
+        }
+    }
+    return val;
+}
+
+template <typename T>
+auto lower_star_filtration(
+    const bats::CubicalComplex& X,
+    const std::vector<std::vector<T>>& f0
+) {
+    std::vector<std::vector<T>> vals(X.maxdim() + 1);
+    // std::vector<std::vector<size_t>> inds(X.maxdim() + 1);
+    for (size_t k = 0; k < X.maxdim() + 1; k++) {
+        vals[k].reserve(X.ncells(k));
+        // inds[k].reserve(X.ncells(k));
+        for (auto& s : X.get_cubes(k)) {
+            // auto [v, i] = f(s);
+            auto v = max_cube_val(s, f0);
+            vals[k].emplace_back(v);
+            // inds[k].emplace_back(i);
+        }
+    }
+    // return std::make_tuple(vals, inds);
+    return vals;
+}
+
+template <typename T>
+auto lower_star_filtration(
+    const bats::CubicalComplex& X,
+    const std::vector<std::vector<std::vector<T>>>& f0
+) {
+    std::vector<std::vector<T>> vals(X.maxdim() + 1);
+    // std::vector<std::vector<size_t>> inds(X.maxdim() + 1);
+    for (size_t k = 0; k < X.maxdim() + 1; k++) {
+        vals[k].reserve(X.ncells(k));
+        // inds[k].reserve(X.ncells(k));
+        for (auto& s : X.get_cubes(k)) {
+            // auto [v, i] = f(s);
+            auto v = max_cube_val(s, f0);
+            vals[k].emplace_back(v);
+            // inds[k].emplace_back(i);
+        }
+    }
+    // return std::make_tuple(vals, inds);
+    return vals;
 }
